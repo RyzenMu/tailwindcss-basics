125998289cba37dd632feefb633ff712b5f225df
RyzenMu <muruga13in2022@gmail.com>
2025-03-25 13:52:51 +0530

    tailwind basics modified

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,12 @@ import tailwindcss from '@tailwindcss/vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    port: process.env.PORT || 3000, // Use Render's provided PORT
+    host: "0.0.0.0" // Allow external access
+  }
 })
+
+
+
+
